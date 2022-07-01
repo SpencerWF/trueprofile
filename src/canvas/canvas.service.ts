98296f92e3020684ede2e21d08d118283f90280d
SpencerWF <spencer.walkerfooks@gmail.com>
2022-07-01 13:27:47 +0100

    Testing of canvas

--- src/canvas/canvas.service.ts
+++ src/canvas/canvas.service.ts
@@ -10,7 +10,7 @@ export const draw_circle_from_url = async (image_url: string) => {
     context.fillStyle = 'Red';
     context.arc(50, 200, 30, 0, Math.PI*2);
     const image_out = canvas.toBuffer("image/png");
-    // writeFileSync("./image_out.png", image_out);
+    writeFileSync("./image_out.png", image_out);
 
     return image_out;
 }
