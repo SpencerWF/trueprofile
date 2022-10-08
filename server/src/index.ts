152b947d8bc99d12e7633bfe14bb3ceb4500cebb
SpencerWF <spencer.walkerfooks@gmail.com>
2022-10-08 21:05:51 +0100

    Change ts config module to es5

--- server/src/index.ts
+++ server/src/index.ts
@@ -14,8 +14,18 @@ import * as profileService from "./profile/profile.service";
 import { profileRouter } from "./profile/profile.router";
 import { init_listener } from "./twitch/twitch.service";
 
+// const { auth } = require('express-openid-connect');
+
+// const auth_config = {
+//     authRequired: false,
+//     auth0logout: true,
+//     secret: "2A8F0DB4CE0408B3174D1C2B5D2EF93E94E0706E26E73C05C773B7C161426362",
+//     baseURL: "http://192.168.1.237:4200/settings-component/",
+//     clientID: "6fx5x7nOozGVzssb23NjG6LWIWleihf4",
+//     issuerBaseURL: "dev-f5zxf23m.eu.auth0.com"
+// }
 // const session = require('express-session');
-var session = require('express-session');
+// var session = require('express-session');
 
 // import * as streamerService from "./streamer/streamer.service";
 
@@ -23,15 +33,18 @@ if(!process.env.PORT) {
     process.exit(1);
 }
 const app = express();
+
+// app.use(auth(auth_config));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '../../trueprofile/dist/'));
 
-app.use(session({
-    secret: process.env.SESSION_SECRET,
-    saveUninitialized: false,
-    resave: false
-}));
+// app.use(session({
+//     secret: process.env.SESSION_SECRET,
+//     saveUninitialized: false,
+//     resave: false
+// }));
 
 init_listener();
 
