0f140d921d4363fe77d2c73a0041bfa072239552
SpencerWF <spencer.walkerfooks@gmail.com>
2022-10-09 15:20:21 +0100

    Log out button in angular application

--- trueprofile/src/app/login-button/login-button.component.ts
+++ trueprofile/src/app/login-button/login-button.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 
 // Import the AuthService type from the SDK
 import { AuthService } from '@auth0/auth0-angular';
 
+import { DOCUMENT } from '@angular/common';
+
 @Component({
   selector: 'app-login-button',
-  template: '<button (click)="auth.loginWithRedirect()">Log in</button>'
+  template: `
+  <ng-container *ngIf="auth.isAuthenticated$ | async; else loggedOut">
+    <button (click)="auth.logout({ returnTo: document.location.origin })">
+      Log out
+    </button>
+  </ng-container>
+
+  <ng-template #loggedOut>
+    <button (click)="auth.loginWithRedirect()">Log in</button>
+  </ng-template>
+  `,
+  styles: [],
 })
 export class LoginButtonComponent implements OnInit {
 
-  constructor(public auth: AuthService) {}
+  constructor(@Inject(DOCUMENT) public document: Document, public auth: AuthService) {}
 
   ngOnInit(): void {
   }
