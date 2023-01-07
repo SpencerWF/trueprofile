d8a57394f21b4c032571a0d8eba38eaa2cc10fd1
Spencer Walker-Fooks <spencer.walkerfooks@gmail.com>
2023-01-07 21:30:20 +0000

    Adding CORS_ORIGIN for test and production

--- src/index.ts
+++ src/index.ts
@@ -29,7 +29,7 @@ const checkJwt = auth({
     issuerBaseURL: process.env.ISSUER_BASE_URL,
 });
 
-const allowedOrigins = [process.env.CORS_ORIGIN];
+const allowedOrigins = [process.env.CORS_TEST_ORIGIN, process.env.CORS_ORIGIN];
 const options: cors.CorsOptions = {
     origin: allowedOrigins
 };
